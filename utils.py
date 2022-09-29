4ae6d750bb1f4f9e748149a574b6afc8541afb1d
n4lkin <kalugin.serej1@gmail.com>
2022-09-29 13:52:24 +0300

    fix: refactor список() &  написать()

--- utils.py
+++ utils.py
@@ -1,16 +1,27 @@
 import random
+import sys
 
-def написать(*args):
-    print(args)
 
-def число(number):
+def написать(*args) -> None:
+    args_ = str([arg for arg in args])
+    sys.stdout.write(args_[1:-1:])
+
+
+def число(number: int) -> int:
     return int(number)
 
-def строка(striing):
+
+def строка(striing: str) -> str:
     return str(striing)
 
-def рандом_рандинт(first: int, second: int):
+
+def рандом_рандинт(first: int, second: int) -> int:
     return random.randint(first, second)
 
-def список():
+
+def список(*args, **kwargs) -> list:
+    if args and kwargs:
+        args_ = [arg for arg in args]
+        kwargs_ = [kwarg for kwarg in kwargs.values()]
+        return args_ + kwargs_
     return list()
